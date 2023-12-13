31e02ea98589c8f3736da562d30a72c9b0aa569d
FlowgeniusJB <146901850+jbeduze@users.noreply.github.com>
2023-12-13 03:51:38 -0600

    Update Team_Roster.py

--- pages/Team_Roster.py
+++ pages/Team_Roster.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import streamlit.components.v1 as components
 import pygwalker as pyg
 
 st.set_page_config(
@@ -8,17 +9,22 @@ st.set_page_config(
   layout="wide",
   initial_sidebar_state="expanded",
 )
+df = pd.read_csv("wrestling_fake_data_Sheet1.csv")
+pyg_html = pyg.walk(df, return_html=True)
 
-#load local data file
-@st.cache
-def load_data():
-  df = pd.read_csv("wrestling_fake_data_Sheet1.csv")
-  return df
+components.html(pyg_html, height=1000, scrolling=True)
+                
+
+# #load local data file
+# @st.cache
+# def load_data():
+#   df = pd.read_csv("wrestling_fake_data_Sheet1.csv")
+#   return df
   
-#set up Pygwalker
-def load_config(file_path):
-  with open(file_path, 'r') as config_file:
-    config_str = config_file.read()
-  return config_str
-config = load_config('config.json')
-pyg.walk(df, env='streamlit', dark='dark', spec=config)
+# #set up Pygwalker
+# def load_config(file_path):
+#   with open(file_path, 'r') as config_file:
+#     config_str = config_file.read()
+#   return config_str
+# config = load_config('config.json')
+# pyg.walk(df, env='streamlit', dark='dark', spec=config)
