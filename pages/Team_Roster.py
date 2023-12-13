7027011b3074c605da97b19c8c0bb699f67edcfa
FlowgeniusJB <146901850+jbeduze@users.noreply.github.com>
2023-12-13 03:26:42 -0600

    Update Team_Roster.py

--- pages/Team_Roster.py
+++ pages/Team_Roster.py
@@ -1,3 +1,12 @@
 import streamlit as st
 
-st.title("Team Roster")
+st/set_page_config(
+  page_title="Team Roster and cross analysis",
+  page_icon=":snake:",
+  layout="wide",
+  initial_sidebar_state="expanaded",
+)
+
+@st.cache_data
+df = pd.read_csv("wrestling_fake_data_Sheet1.csv")
+  
