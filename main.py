847b2372dede2a64a5f1362687c1cbeb71a64f89
FlowgeniusJB <146901850+jbeduze@users.noreply.github.com>
2023-12-13 02:58:56 -0600

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import pygwalker as pyg
 
 
 st.write('Welcome to your sports dashboard')
-uploader_file = st.file_uploader("choose a file or drag and drop")
+uploaded_file = st.file_uploader("choose a file or drag and drop")
   if uploaded_file is not None:
     dataframe = pd.read_csv(uploaded_file)
     st.write(dataframe)
