2895d1131013d739303abaa5531811b43674288e
FlowgeniusJB <146901850+jbeduze@users.noreply.github.com>
2023-12-13 03:30:11 -0600

    Update Team_Roster.py

--- pages/Team_Roster.py
+++ pages/Team_Roster.py
@@ -6,7 +6,14 @@ st/set_page_config(
   layout="wide",
   initial_sidebar_state="expanaded",
 )
-
+#load local data file
 @st.cache_data
 df = pd.read_csv("wrestling_fake_data_Sheet1.csv")
   
+#set up Pygwalker
+def load_config(file_path):
+  with open(file_path, 'r') as config_file:
+    config_str = config_file.read()
+  return config_str
+config = load_config('config.json')
+pyg.walk(df, env='streamlit', dark='dark', spec=config)
