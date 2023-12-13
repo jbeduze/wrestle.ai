7807b457b1e73f760b2a0bdbc77134483f137db4
FlowgeniusJB <146901850+jbeduze@users.noreply.github.com>
2023-12-13 03:06:25 -0600

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ import pygwalker as pyg
 st.write('Welcome to your sports dashboard')
 #uploaded_file = st.file_uploader("choose a file or drag and drop")
   #if uploaded_file is not None:
-df = pd.read_csv(wrestling fake data - Sheet1.csv) #(uploaded_file)
+df = pd.read_csv(wrestling_fake_data_Sheet1.csv) #(uploaded_file)
 df.head()
 pyg.walk(df, dark='light')
