71e4f8eb71215f12d706864604333979cf3caac0
FlowgeniusJB <146901850+jbeduze@users.noreply.github.com>
2023-12-14 00:45:43 -0600

    Update main.py

--- main.py
+++ main.py
@@ -10,13 +10,12 @@ st.write('Welcome to your sports dashboard')
 df = pd.read_csv("wrestling_fake_data_Sheet1.csv") #(uploaded_file)
 
 
-# video_file = st.video()
-# video_bytes = video_file.read()
-
-# st.video(video_bytes)
-
+video_files = st.file_uploader("Upload a video file", type=['.mp4', '.avi', '.mov', '.mkv'], accept_multiple_files=True)
+for video_file in video_files:
+  video_bytes = video_file.read()
+  st.video(video_bytes)
+"---"
 uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
 for uploaded_file in uploaded_files:
     bytes_data = uploaded_file.read()
-    st.write("filename:", uploaded_file.name)
     st.write(bytes_data)
