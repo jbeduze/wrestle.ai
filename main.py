0fc799348d86ec5e17100380afe84a30cc7e89b9
FlowgeniusJB <146901850+jbeduze@users.noreply.github.com>
2023-12-14 02:27:58 -0600

    Update main.py

--- main.py
+++ main.py
@@ -12,46 +12,7 @@ import os
 import requests
 
 #client = OpenAI()
-st.write('Welcome to your sports dashboard')
-"---"
-# video_files = st.file_uploader("Upload a video file", type=['.mp4', '.avi', '.mov', '.mkv'], accept_multiple_files=True)
-
-# for video_file in video_files:
-#     # Save the uploaded video file to a temporary file
-#     with tempfile.NamedTemporaryFile(delete=False, suffix='.mp4') as tmp_file:
-#         tmp_file.write(video_file.read())
-#         tmp_file_path = tmp_file.name
-
-#     # Display the video
-#     st.video(tmp_file_path)
-
-#     # Load the video using OpenCV
-#     video = cv2.VideoCapture(tmp_file_path)
-
-#     # Get the total number of frames
-#     total_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
-
-#     # Define the slider with the total number of frames as the max value
-#     frame_number = st.slider('Select a frame', 0, total_frames - 1)
-
-#     # Processing the video
-#     base64Frames = []
-#     frame_idx = 0
-#     while video.isOpened():
-#         success, frame = video.read()
-#         if not success:
-#             break
-#         _, buffer = cv2.imencode(".jpg", frame)
-#         base64Frames.append(base64.b64encode(buffer).decode("utf-8"))
-#         frame_idx += 1
-#         if frame_idx > frame_number:
-#             break
-
-#     video.release()
-#     print(len(base64Frames), "frames read.")
-
-
-st.write('Welcome to your sports dashboard')
+st.write('Welcome to your Athlete Analysis dashboard')
 
 video_files = st.file_uploader("Upload a video file", type=['.mp4', '.avi', '.mov', '.mkv'], accept_multiple_files=True)
 
