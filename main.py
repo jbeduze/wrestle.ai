f09ce06d76d21b53ad8e2f2a2beac6e0d2cfd868
FlowgeniusJB <146901850+jbeduze@users.noreply.github.com>
2023-12-14 01:15:56 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import streamlit as st
 import plotly
 import pandas as pd
 import pygwalker as pyg
-from IPython.display import display, Image, Audio
 
+from IPython.display import display, Image, Audio
 import cv2  # We're using OpenCV to read video, to install !pip install opencv-python
 import base64
 import time
@@ -16,7 +16,7 @@ client = OpenAI()
 st.write('Welcome to your sports dashboard')
 #uploaded_file = st.file_uploader("choose a file or drag and drop")
   #if uploaded_file is not None:
-df = pd.read_csv("wrestling_fake_data_Sheet1.csv") #(uploaded_file)
+#df = pd.read_csv("wrestling_fake_data_Sheet1.csv") #(uploaded_file)
 
 
 video_files = st.file_uploader("Upload a video file", type=['.mp4', '.avi', '.mov', '.mkv'], accept_multiple_files=True)
@@ -29,3 +29,15 @@ for video_file in video_files:
 #     bytes_data = uploaded_file.read()
 #     st.write(bytes_data)
 
+video = cv2.VideoCapture("data/bison.mp4")
+
+base64Frames = []
+while video.isOpened():
+    success, frame = video.read()
+    if not success:
+        break
+    _, buffer = cv2.imencode(".jpg", frame)
+    base64Frames.append(base64.b64encode(buffer).decode("utf-8"))
+
+video.release()
+print(len(base64Frames), "frames read.")
