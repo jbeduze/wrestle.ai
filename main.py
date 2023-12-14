a06545d9c95cdfd7c518bd0f16020c4e168eedd9
FlowgeniusJB <146901850+jbeduze@users.noreply.github.com>
2023-12-14 02:20:13 -0600

    Update main.py

--- main.py
+++ main.py
@@ -13,10 +13,10 @@ import requests
 
 st.write('Welcome to your sports dashboard')
 
-video_files = st.file_uploader("Upload a video file", type=['.mp4', '.avi', '.mov', '.mkv'], accept_multiple_files=True)
-for video_file in video_files:
-  video_bytes = video_file.read()
-  st.video(video_bytes)
+# video_files = st.file_uploader("Upload a video file", type=['.mp4', '.avi', '.mov', '.mkv'], accept_multiple_files=True)
+# for video_file in video_files:
+#   video_bytes = video_file.read()
+#   st.video(video_bytes)
 
 st.write("Read video uploaded, establish keyframes, provide description of what's happening in video")
 "---"
@@ -29,9 +29,7 @@ st.write("Read video uploaded, establish keyframes, provide description of what'
       #extract the Frames from uploaded video
       # Load the video
 
-import streamlit as st
-import cv2
-import base64
+
 import tempfile
 
 st.write('Welcome to your sports dashboard')
