fb8e624dee706b8b0dca4f972082efbe9b981290
FlowgeniusJB <146901850+jbeduze@users.noreply.github.com>
2023-12-14 00:36:56 -0600

    Update main.py

--- main.py
+++ main.py
@@ -8,5 +8,10 @@ st.write('Welcome to your sports dashboard')
 #uploaded_file = st.file_uploader("choose a file or drag and drop")
   #if uploaded_file is not None:
 df = pd.read_csv("wrestling_fake_data_Sheet1.csv") #(uploaded_file)
-df.head()
-pyg.walk(df)
+
+
+video_file = st.video('Upload Video Here',
+                     type=['.mp4', '.avi', '.mov', '.mkv'])
+video_bytes = video_file.read()
+
+st.video(video_bytes)
