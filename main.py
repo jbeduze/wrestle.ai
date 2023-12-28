db147a367f26f039bd8373a4c5e1c1c95cdbf6bb
FlowgeniusJB <146901850+jbeduze@users.noreply.github.com>
2023-12-28 01:52:37 -0600

    Update main.py

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ out = cv2.VideoWriter('output.mp4', fourcc, fps, (int(video.get(3)), int(video.g
 for _ in range(start_frame, end_frame):
         ret, frame = video.read()
         if not ret:
-	break
+break
 	out.write(frame)
 
 out.release()
