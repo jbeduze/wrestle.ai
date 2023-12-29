e63447270e969d199d328f5b17372d374fd58c3e
FlowgeniusJB <146901850+jbeduze@users.noreply.github.com>
2023-12-28 23:42:01 -0600

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,10 @@ import cv2
 import tempfile
 import urllib.request
 
-# ... [rest of your imports]
+#facial recognition
+haar_url = "https://github.com/opencv/opencv/blob/master/data/haarcascades/haarcascade_frontalface_default.xml?raw=true"
+haar_file = "haarcascade_frontalface_default.xml"
+urllib.request.urlretrieve(haar_url, haar_file)
 
 st.write('Welcome to your Athlete Analysis dashboard')
 st.write("If you choose to upload files into this software or take live videos, all is possible!")
@@ -41,40 +44,38 @@ for video_file in video_files:
     # Display the video
     st.video(tmp_file_path)
 
-    # Load the video using OpenCV
-    video = cv2.VideoCapture(tmp_file_path)
+    # # Load the video using OpenCV
+    # video = cv2.VideoCapture(tmp_file_path)
 
-    # Get the total number of frames and calculate duration
-    total_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
-    fps = video.get(cv2.CAP_PROP_FPS)
-    duration = total_frames / fps
+    # # Get the total number of frames and calculate duration
+    # total_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+    # fps = video.get(cv2.CAP_PROP_FPS)
+    # duration = total_frames / fps
 
-    # Define the slider with the total number of frames as the max value and the range within that you want to grab
-    start_time = st.slider("Start Time (seconds)", 0.0, duration, 0.0, 0.1, key=f"{video_file.name}_start")
-    end_time = st.slider("End Time (seconds)", 0.0, duration, duration, 0.1, key=f"{video_file.name}_end")
+    # # Define the slider with the total number of frames as the max value and the range within that you want to grab
+    # start_time = st.slider("Start Time (seconds)", 0.0, duration, 0.0, 0.1, key=f"{video_file.name}_start")
+    # end_time = st.slider("End Time (seconds)", 0.0, duration, duration, 0.1, key=f"{video_file.name}_end")
 
-    if st.button('Extract Video Segment', key=f"{video_file.name}_extract"):
-        start_frame = int(start_time * fps)
-        end_frame = int(end_time * fps)
-        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-        out = cv2.VideoWriter('output.mp4', fourcc, fps, (int(video.get(3)), int(video.get(4))))
+    # if st.button('Extract Video Segment', key=f"{video_file.name}_extract"):
+    #     start_frame = int(start_time * fps)
+    #     end_frame = int(end_time * fps)
+    #     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    #     out = cv2.VideoWriter('output.mp4', fourcc, fps, (int(video.get(3)), int(video.get(4))))
 
-        video.set(cv2.CAP_PROP_POS_FRAMES, start_frame)
+    #     video.set(cv2.CAP_PROP_POS_FRAMES, start_frame)
 
-        for _ in range(start_frame, end_frame):
-            ret, frame = video.read()
-            if not ret:
-                break
-            out.write(frame)
+    #     for _ in range(start_frame, end_frame):
+    #         ret, frame = video.read()
+    #         if not ret:
+    #             break
+    #         out.write(frame)
 
-        out.release()
-        st.video('output.mp4')
+    #     out.release()
+    #     st.video('output.mp4')
 
-    video.release()
+    # video.release()
 
-haar_url = "https://github.com/opencv/opencv/blob/master/data/haarcascades/haarcascade_frontalface_default.xml?raw=true"
-haar_file = "haarcascade_frontalface_default.xml"
-urllib.request.urlretrieve(haar_url, haar_file)
+
 
 # # ... [rest of your imports]
 
