8a53cce43a0b68c477f50d3597e561c86f4f0299
FlowgeniusJB <146901850+jbeduze@users.noreply.github.com>
2024-03-26 19:46:33 -0500

    Update 1_Dev_1.py

--- pages/1_Dev_1.py
+++ pages/1_Dev_1.py
@@ -4,7 +4,7 @@ import pandas as pd
 import pygwalker as pyg
 import tempfile
 from IPython.display import display, Image, Audio
-from movie.editor import as VideoFileClip
+from movie.editor import VideoFileClip
 import cv2
 import base64
 import time
