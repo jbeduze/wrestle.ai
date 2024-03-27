c751e6d142911d03323f6a40c2bcc0c38ab02560
FlowgeniusJB <146901850+jbeduze@users.noreply.github.com>
2024-03-26 20:53:57 -0500

    Update 2_dev_2.py

--- pages/2_dev_2.py
+++ pages/2_dev_2.py
@@ -22,9 +22,9 @@ if video_files:
         total_frames = int(clip.fps * clip.duration)
 
 
-   #1st expander for the seconds to be displayed     
+#1st expander for the seconds to be displayed     
         with st.expander(f"Select Frame Range of the {total_frames} from the upload"):
-            # Define the range slider for selecting start and end times within the expander
+# Define the range slider for selecting start and end times within the expander
             start_time, end_time = st.slider("Time Range (seconds)", 0.0, clip.duration, (0.0, clip.duration))
 
             if st.button("Extract Video Segment from original"):
@@ -39,11 +39,10 @@ if video_files:
                     st.video(segment_file.name)
 
             else: st.warning("Please select a range to create a video segment")
-  #2nd expander for the frames to be displayed              
+#2nd expander for the frames to be displayed              
         with st.expander(f"Select Frame Range of the {total_frames} from the upload"):
-            # Define the range slider for selecting start and end times within the expander
-            start_time, end_time = st.slider("Frame Range (Frames)", 0.0, clip.duration, (0.0, clip.duration))
-
+# Define the range slider for selecting start and end Frames within the expander
+            start_frame, end_frame = st.slider("Frame Range", 0, total_frames, (0, total_frames))
             if st.button("Extract Video Segment from original"):
                 # Extract the segment
                 segment_clip = clip.subclip(start_time, end_time)
