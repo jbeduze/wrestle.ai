0201e81227019dd404953ed08be1c6c79d2026bb
FlowgeniusJB <146901850+jbeduze@users.noreply.github.com>
2024-03-26 20:52:30 -0500

    Update 2_dev_2.py

--- pages/2_dev_2.py
+++ pages/2_dev_2.py
@@ -21,11 +21,13 @@ if video_files:
         clip = VideoFileClip(tmp_file_path)
         total_frames = int(clip.fps * clip.duration)
 
+
+   #1st expander for the seconds to be displayed     
         with st.expander(f"Select Frame Range of the {total_frames} from the upload"):
             # Define the range slider for selecting start and end times within the expander
             start_time, end_time = st.slider("Time Range (seconds)", 0.0, clip.duration, (0.0, clip.duration))
 
-            if st.button("Extract Video Segment"):
+            if st.button("Extract Video Segment from original"):
                 # Extract the segment
                 segment_clip = clip.subclip(start_time, end_time)
                 
@@ -35,3 +37,22 @@ if video_files:
 
                     # Display the segment
                     st.video(segment_file.name)
+
+            else: st.warning("Please select a range to create a video segment")
+  #2nd expander for the frames to be displayed              
+        with st.expander(f"Select Frame Range of the {total_frames} from the upload"):
+            # Define the range slider for selecting start and end times within the expander
+            start_time, end_time = st.slider("Frame Range (Frames)", 0.0, clip.duration, (0.0, clip.duration))
+
+            if st.button("Extract Video Segment from original"):
+                # Extract the segment
+                segment_clip = clip.subclip(start_time, end_time)
+                
+                # Save the segment to a temporary file
+                with tempfile.NamedTemporaryFile(delete=False, suffix='.mp4') as segment_file:
+                    segment_clip.write_videofile(segment_file.name, codec="libx264", audio_codec="aac")
+
+                    # Display the segment
+                    st.video(segment_file.name)
+
+            else: st.warning("Please select a range to create a video segment")
