172426e2716170c0fd4290ecbc32d83c0df216dc
FlowgeniusJB <146901850+jbeduze@users.noreply.github.com>
2024-04-18 00:47:57 -0500

    Update 8_CAreader.py

--- pages/8_CAreader.py
+++ pages/8_CAreader.py
@@ -1,2 +1,16 @@
 #required libraries
 #streamlit, python, streamlit-extras, PyPFD, faiss, langchain
+#tutorial: https://www.youtube.com/watch?v=RIWbalZ7sTo&t=475s
+
+import streamlit as st
+
+
+st.header("Credit application processing")
+st.markdown('''
+this will do the following:
+-End goal is to import doc.s and pull the important info and display it in an easier fashion
+-this will drop the necsary info into an internal doc
+''')
+add_vertical_space(5)
+("---")
+st.write('langchain will be the main engine of this processing')
